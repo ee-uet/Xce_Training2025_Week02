2c65af4dcd82003031ffef9f5e43537e93348acd
Muhammad Boota <boota09299@gmail.com>
2025-09-13 04:31:17 +0500

    An error in spi module resolved

--- Muhammad_Boota/lab9/src/spi_master.sv
+++ Muhammad_Boota/lab9/src/spi_master.sv
@@ -21,12 +21,13 @@ input logic spi_miso,
 output logic [NUM_SLAVES-1:0] spi_cs_n
 );
 // TOD: Implement SPI master
+logic spi_clk_n;
 logic [$clog2(DATA_WIDTH):0]bit_count,bit_count_n;
-logic [$clog2(NUM_SLAVES)-1:0]slave_sel_reg;
-logic [DATA_WIDTH-1:0]shift_reg_tx,shift_reg_rx;
+logic [$clog2(NUM_SLAVES)-1:0]slave_sel_reg,slave_sel_reg_n;
+logic [DATA_WIDTH-1:0]shift_reg_tx,shift_reg_tx_n,shift_reg_rx,shift_reg_rx_n;
 logic [15:0]clk_count,clk_count_n;
-logic drive_edge,sample_edge,cpha_reg,cpol_reg;
-logic [15:0] clk_div_reg;
+logic drive_edge,drive_edge_n,sample_edge,sample_edge_n,cpha_reg,cpha_reg_n,cpol_reg,cpol_reg_n;
+logic [15:0] clk_div_reg,clk_div_reg_n;
 spi_state curr_state,next_state;
 
 always_ff @( posedge clk ) begin : blockName
@@ -44,7 +45,7 @@ always_comb begin
             if (start_transfer)
                 next_state = SETUP;
             else 
-                next_state = SETUP;
+                next_state = IDLE;
         end
         SETUP: begin
             next_state = TRANSFER;
@@ -63,94 +64,118 @@ always_comb begin
 
 
     always_comb begin
+        slave_sel_reg_n =slave_sel_reg   ;
+        shift_reg_rx_n  =shift_reg_rx    ;
+        sample_edge_n   =sample_edge     ;
+        drive_edge_n    =drive_edge      ;
+        cpha_reg_n      =cpha_reg        ;
+        cpol_reg_n      =cpol_reg        ;
+        shift_reg_tx_n  =shift_reg_tx    ;
+        clk_div_reg_n   =clk_div_reg     ;
+        spi_clk_n       =spi_clk         ;
+        bit_count_n     =bit_count       ;
+        clk_count_n     =clk_count       ;
         spi_cs_n = '1; // All CS_N high by default
         transfer_done = 1'b0;
-        rx_data =0;
         case (curr_state)
+            IDLE: begin
+                busy = 1'b0;
+                slave_sel_reg_n =slave_sel;
+                cpha_reg_n      =cpha;
+                cpol_reg_n      =cpol;
+                shift_reg_tx_n  =tx_data; // Load transmit data
+                clk_div_reg_n   =clk_div;
+                spi_clk_n       =cpol; // Idle state of clock
+                sample_edge_n   =0;
+                drive_edge_n    =0;
+                bit_count_n     =0;
+                clk_count_n     =0;
+            end
             SETUP: begin
+                spi_clk_n       =cpol_reg;
                 busy = 1'b1;
                 spi_cs_n[slave_sel_reg] = 1'b0;
             end
             TRANSFER: begin
                 busy = 1'b1;
                 spi_cs_n[slave_sel_reg] = 1'b0;
+                case ({cpol_reg,cpha_reg})
+                    2'b00:spi_clk_n = (clk_count < (clk_div_reg>>1)) ? 1'b1 : 1'b0;
+                    2'b01:spi_clk_n = (clk_count < (clk_div_reg>>1)) ? 1'b0 : 1'b1;
+                    2'b10:spi_clk_n = (clk_count < (clk_div_reg>>1)) ? 1'b0 : 1'b1;
+                    2'b11:spi_clk_n = (clk_count < (clk_div_reg>>1)) ? 1'b1 : 1'b0;
+                    default: spi_clk_n = cpol_reg;
+                endcase
+                clk_count_n=(clk_count==(clk_div_reg)) ? 0 : clk_count+1;
+                bit_count_n= (sample_edge) ? bit_count+1 : bit_count;
+                // Consider: How to handle different CPOL/CPHA modes?
+                // Edge detection for sampling and driving
+                drive_edge_n = (clk_count == 0 ) ? 1'b1 : 1'b0;
+                sample_edge_n= (clk_count == (clk_div_reg>>1) ) ? 1'b1 : 1'b0;
+                // Shift out MOSI
+                shift_reg_tx_n = (drive_edge) ? {shift_reg_tx[DATA_WIDTH-2:0], 1'b0} : shift_reg_tx;
+                // Shift in MISO
+                shift_reg_rx_n = (sample_edge) ? {shift_reg_rx[DATA_WIDTH-2:0], spi_miso}:shift_reg_rx;
+                bit_count_n= (sample_edge) ? bit_count+1 : bit_count;
             end
             COMPLETE: begin
                 transfer_done = 1'b1;
-                busy = 1'b0;
+                busy = 1'b1;
+                spi_clk_n=cpol_reg;
                 spi_cs_n[slave_sel_reg] = 1'b0; 
-                rx_data = shift_reg_rx; // Output received data
             end
         endcase
     end
 
-always_ff @( posedge clk ) begin
-    if (!rst_n) begin
-        cpha_reg      <=0;
-        cpol_reg      <=0;
-        spi_clk       <=0;
-        shift_reg_tx  <=0;
-        clk_div_reg   <=0;
-        spi_mosi      <=0;
-        clk_count     <=0;
-        bit_count     <=0;
-        shift_reg_rx  <=0;
-         slave_sel_reg<=0;
-    end else begin
-        case (curr_state)
-            SETUP:begin
-                slave_sel_reg <=slave_sel;
-                cpha_reg      <=cpha;
-                cpol_reg      <=cpol;
-                shift_reg_tx  <= tx_data; // Load transmit data
-                clk_div_reg   <= clk_div;
-                spi_clk       <=cpha;
-                bit_count     <=0;
-                clk_count     <=0;
-                end
-            TRANSFER:begin
-                clk_count    <=clk_count_n;
-                if (drive_edge) begin
-                    // Shift out MOSI
-                    spi_mosi <= shift_reg_tx[DATA_WIDTH-1];
-                    shift_reg_tx <= {shift_reg_tx[DATA_WIDTH-2:0], 1'b0};
-                end
-                if (sample_edge) begin
-                    // Shift in MISO
-                    shift_reg_rx <= {shift_reg_rx[DATA_WIDTH-2:0], spi_miso};
-                    bit_count    <=bit_count_n;
-                end
-
-                if (clk_count==(clk_div_reg)) 
-                    spi_clk <=~spi_clk;
-            end
-            default: begin end
-        endcase
-    end
+always_ff @(posedge clk) begin
+        if (!rst_n) begin
+            slave_sel_reg <=0;
+            cpha_reg      <=0;
+            cpol_reg      <=0;
+            shift_reg_tx  <=0;
+            sample_edge   <=0;
+            drive_edge    <=0;
+            shift_reg_rx  <=0;
+            clk_div_reg   <=0;
+        end else begin
+            slave_sel_reg <=slave_sel_reg_n   ;
+            sample_edge   <=sample_edge_n     ;
+            drive_edge    <=drive_edge_n      ;
+            cpha_reg      <=cpha_reg_n        ;
+            cpol_reg      <=cpol_reg_n        ;
+            shift_reg_tx  <=shift_reg_tx_n    ;
+            shift_reg_rx  <=shift_reg_rx_n    ;
+            clk_div_reg   <=clk_div_reg_n     ;
+        end
 end
 
 //receiving bit counter
-always_comb begin 
-        bit_count_n=bit_count+1;
+always_ff @(posedge clk) begin 
+        if (!rst_n)
+            bit_count <=0;
+        else 
+            bit_count <= bit_count_n; 
 end
 
 //clk generation
-
-always_comb begin 
-    if (clk_count==(clk_div_reg))
-        clk_count_n=0;
+always_ff @( posedge clk ) begin 
+    if (!rst_n)
+        clk_count <=0;
     else 
-        clk_count_n=clk_count+1;
+        clk_count <= clk_count_n; 
+    
 end
-// Consider: How to handle different CPOL/CPHA modes?
-    // Edge detection for sampling and driving
-always_comb begin 
-    if (cpol_reg) begin
-        sample_edge = (cpol_reg == cpha_reg) ? ~spi_clk  : spi_clk ;
-        drive_edge  = (cpol_reg == cpha_reg) ? spi_clk  : ~spi_clk ;
+//seding data
+always_ff @( posedge drive_edge ) begin 
+    spi_mosi <= shift_reg_tx[DATA_WIDTH-1];
+end
+
+always_ff @( posedge clk ) begin 
+    if (!rst_n) begin
+        spi_clk=0;
     end else begin
-        drive_edge   = (cpol_reg == cpha_reg) ? ~spi_clk  : spi_clk ;
-        sample_edge  = (cpol_reg == cpha_reg) ? spi_clk  : ~spi_clk ;
+        spi_clk=spi_clk_n;
     end
 end 
+assign rx_data = shift_reg_rx;
 endmodule
--- Muhammad_Boota/lab9/tests/spi_master_tb.sv
+++ Muhammad_Boota/lab9/tests/spi_master_tb.sv
@@ -1,5 +1,5 @@
 import pkg::*;
-module spi_master_tb #( TESTS=100,
+module spi_master_tb #( TESTS=1000,
 parameter int NUM_SLAVES = 4,
 parameter int DATA_WIDTH = 8
 )();
@@ -35,21 +35,119 @@ initial begin
     rst_n=1;
     @(posedge(clk));
     for (int i =0 ;i<TESTS ;i++ ) begin
-        tx_data=$urandom_range(0,256);
         slave_sel=$urandom_range(0,3);
         cpha=$urandom_range(0,1);
         cpol=$urandom_range(0,1);
-        clk_div=$urandom_range(0,8);
-        @(posedge (clk));
-        start_transfer=1;
-        while(!transfer_done)begin
-            spi_miso=$urandom_range(0,1);
-            @(posedge clk);
+        clk_div=$urandom_range(4,8);
+        @(posedge(clk));
+        fork
+            miso_test(i);
+            mosi_test(i);
+        join
+        @(posedge(clk));
         end
-        start_transfer=0;
-        @(posedge clk);
+        $finish;
     end
-    $finish;
-end
+    
+task automatic miso_test(input int test);
+        logic [DATA_WIDTH-1:0] data;
+        data=$urandom_range(0,256);
+        @(posedge(clk));
+        start_transfer=1;
+        @(posedge(clk));
+        start_transfer=0;
+        case ({cpol,cpha})
+            2'b00:begin
+                for (int i = 8;i>0 ;i-- ) begin
+                    @(posedge (spi_clk));
+                    spi_miso=data[i-1];
+                end
+            end
+            2'b10:begin
+                for (int i = 8;i>0 ;i-- ) begin
+                    @(negedge (spi_clk));
+                    spi_miso=data[i-1];
+                end
+            end
+            2'b01:begin
+                for (int i = 8;i>0 ;i-- ) begin
+                    spi_miso=data[i-1];
+                    @(negedge (spi_clk));
+                end
+            end
+            2'b11:begin
+                for (int i = 8;i>0 ;i-- ) begin
+                    spi_miso=data[i-1];
+                    @(posedge (spi_clk));
+                end
+            end
+            default: begin
+                $error("Invalid CPOL/CPHA combination");
+                $stop;
+            end
+        endcase
+        @(posedge(clk));
+        while(busy)
+            @(posedge clk);
+        @(posedge(clk));
+        if (data !== rx_data) begin
+            $error("MISO data mismatch: expected %0h, got %0h",data,rx_data);
+            $stop;
+        end else begin
+            $display("Test:%d passed,MISO data match: expected %0h, got %0h",test,data,rx_data);
+        end
+endtask //automatic
 
+
+task automatic mosi_test(input int test);
+        logic [DATA_WIDTH-1:0] data,received_data;
+        data=$urandom_range(0,256);
+        @(posedge(clk));
+        tx_data=data;
+        start_transfer=1;
+        @(posedge(clk));
+        start_transfer=0;
+        case ({cpol,cpha})
+            2'b00:begin
+                for (int i = 8;i>0 ;i-- ) begin
+                @(posedge (spi_clk));
+                #1;
+                received_data[i-1]=spi_mosi;
+        end
+            end
+            2'b10:begin
+                for (int i = 8;i>0 ;i-- ) begin
+                @(negedge (spi_clk));
+                #1;
+                received_data[i-1]=spi_mosi;
+        end
+            end
+            2'b01:begin
+                for (int i = 8;i>0 ;i-- ) begin
+                @(posedge (spi_clk));
+                received_data[i-1]=spi_mosi;
+        end
+            end
+            2'b11:begin
+                for (int i = 8;i>0 ;i-- ) begin
+                    @(negedge (spi_clk));
+                    received_data[i-1]=spi_mosi;
+                end
+            end
+            default: begin
+                $error("Invalid CPOL/CPHA combination");
+                $stop;
+            end
+        endcase
+        @(posedge(clk));
+        while(busy)
+            @(posedge clk);
+        @(posedge(clk));
+        if (data !== received_data) begin
+            $error("MOSI data mismatch: expected %0h, got %0h",data,received_data);
+            $stop;
+        end else begin
+            $display("Test:%d passed,MOSI data match: expected %0h, got %0h",test,data,received_data);
+        end
+endtask //automatic
 endmodule
